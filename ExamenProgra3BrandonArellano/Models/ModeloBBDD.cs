45229106f4bda3ab600f0f9053844b855b93c0ae
Brandon Arellano <arebrandon12@gmail.com>
2025-01-20 16:37:55 -0500

    ModeloBBDD

--- ExamenProgra3BrandonArellano/Models/ModeloBBDD.cs
+++ ExamenProgra3BrandonArellano/Models/ModeloBBDD.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using SQLite;
 
 namespace ExamenProgra3BrandonArellano.Models
 {
-    internal class ModeloBBDD
+    public class ModeloBBDD
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string NombreOficial { get; set; }
+        public string Region { get; set; }
+        public string LinkGoogleMaps { get; set; }
+        public string Usuario { get; set; }
+
     }
 }
