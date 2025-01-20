93a6914a49f03518c7c96c8c73a9a0e778a58a6e
Brandon Arellano <arebrandon12@gmail.com>
2025-01-20 16:25:06 -0500

    ModeloAPI para hacer la consulta al API

--- ExamenProgra3BrandonArellano/Models/ModeloAPI.cs
+++ ExamenProgra3BrandonArellano/Models/ModeloAPI.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ExamenProgra3BrandonArellano.Models
 {
-    internal class ModeloAPI
+    public class ModeloAPI
     {
+        public Name name { get; set; }
+        public string region { get; set; }
+        public Maps maps { get; set; }
+    }
+    public class Name
+    {
+        public string common { get; set; }
+        public string official { get; set; }
+        public NativeName nativeName { get; set; }
+    }
+    public class NativeName
+    {
+        public Spa spa { get; set; }
+    }
+
+    public class Spa
+    {
+        public string official { get; set; }
+        public string common { get; set; }
+    }
+
+    public class Maps
+    {
+        public string googleMaps { get; set; }
+        public string openStreetMaps { get; set; }
     }
 }
